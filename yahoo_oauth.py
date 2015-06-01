59d90b0ca36d1048404980bb7573d48daaa95382
josuebrunel <josuebrunel@gmail.com>
2015-06-01 17:20:49 +0200

    fix #4: json_get_data OK

--- yahoo_oauth.py
+++ yahoo_oauth.py
@@ -15,16 +15,21 @@ logging.basicConfig(level=logging.DEBUG, format="[%(asctime)s %(levelname)s] [%(
 logging.getLogger('yahoo-oauth')
 
 
-#######################################
-#
-#           WRITE JSON DATA
-#
-#######################################
 def json_write_data(json_data, filename):
-    
+    """Write json data into a file
+    """
     with open(filename, 'w') as fp:
-        json.dump(json_ddata, fp, indent=4, sort_key=True, ensure_ascii=False)
+        json.dump(json_data, fp, indent=4, sort_keys=True, ensure_ascii=False)
         return True
+    return False
+
+def json_get_data(filename):
+    """Get data from json file
+    """
+    with open(filename) as fp:
+        json_data = json.load(fp)
+        return json_data
+
     return False
 
 
