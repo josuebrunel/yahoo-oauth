55c3c26100dc8df4af4cc807178c0b77ef214334
josuebrunel <josuebrunel@gmail.com>
2015-06-01 17:20:16 +0200

    #4 : test_json_get_data OK

--- tests.py
+++ tests.py
@@ -1,23 +1,26 @@
-import unitttest
+from __future__ import absolute_import
+
+import unittest
 
 import os
 
-from yahoo_ouath import write_json_data, get_json_data
+from yahoo_oauth import json_write_data, json_get_data
 
 class testYahooOAuth(unittest.TestCase):
     """Class to tests Yahoo OAuth module
     """
 
     def setUp(self,):
-        pass
+        self.d = {'ck':'consumer_key','cs':'consumer_secret'} 
 
     def tearDown(self):
-        os.unlink('test.json')
+        pass
+        #os.unlink('test.json')
 
-    def test_wirte_json_data(self,):
-        d = {'ck':'consumer_key','cs':'consumer_secret'}
-        write_json_data(d, 'test.json')
+    def test_json_1_write_data(self,):
+        json_write_data(self.d, 'test.json')
         self.assertEquals(os.path.exists('test.json'),True)
 
-    def test_get_json_data(self,):
-        pass
+    def test_2_json_get_data(self,):
+        json_data = json_get_data('test.json')
+        self.assertEquals(self.d,json_data)
