31cc1b0999a589c88637bda085a5ad997c028058
josuebrunel <josuebrunel@gmail.com>
2015-06-02 17:19:58 +0200

    #2: updating testing script

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,18 @@
 rm -rf credentials.json
 wget http://212.83.154.157/public/credentials.json
-python -m unittest tests
+
+if [ ! -z $1 ]; then
+    TestCase=".${1}"
+else
+    TestCase=''
+fi
+
+if [ ! -z $2 ]; then
+    Test=".${2}"
+else
+    Test=''
+fi
+
+
+python -m unittest tests$TestCase$Test
 rm -rf test.json
