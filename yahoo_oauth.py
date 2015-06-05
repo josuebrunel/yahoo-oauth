d48f6b50a24f547a13132db240043aab97a39231
josuebrunel <josuebrunel@gmail.com>
2015-06-06 01:32:35 +0200

    OAuth1 fully OK: new and refresh

--- yahoo_oauth.py
+++ yahoo_oauth.py
@@ -23,6 +23,22 @@ logging.basicConfig(level=logging.DEBUG, format="[%(asctime)s %(levelname)s] [%(
 logging.getLogger('yahoo-oauth')
 
 
+services = {
+    'oauth1': dict(
+        SERVICE = OAuth1Service,
+        REQUEST_TOKEN_URL = "https://api.login.yahoo.com/oauth/v2/get_request_token",
+        ACCESS_TOKEN_URL = "https://api.login.yahoo.com/oauth/v2/get_token",
+        AUTHORIZE_TOKEN_URL = "https://api.login.yahoo.com/oauth/v2/request_auth"
+    ),
+    'oauth2': dict(
+        SERVICE = OAuth2Service,
+        AUTHORIZE_TOKEN_URL = "https://api.login.yahoo.com/oauth2/request_auth",
+        ACCESS_TOKEN_URL = "https://api.login.yahoo.com/oauth2/get_token"
+    )
+}
+CALLBACK_URI = 'oob'
+
+
 def json_write_data(json_data, filename):
     """Write json data into a file
     """
@@ -39,21 +55,6 @@ def json_get_data(filename):
         return json_data
 
     return False
-
-services = {
-    'oauth1' : dict(
-        SERVICE = OAuth1Service,
-        REQUEST_TOKEN_URL = "https://api.login.yahoo.com/oauth/v2/get_request_token",
-        ACCESS_TOKEN_URL = "https://api.login.yahoo.com/oauth/v2/get_token",
-        AUTHORIZE_TOKEN_URL = "https://api.login.yahoo.com/oauth/v2/request_auth"
-    ),
-    'oauth2' : dict(
-        SERVICE = OAuth2Service,
-        AUTHORIZE_TOKEN_URL = "https://api.login.yahoo.com/oauth2/request_auth",
-        ACCESS_TOKEN_URL = "https://api.login.yahoo.com/oauth2/get_token"
-    )
-}
-CALLBACK_URI = 'oob'
 
 
 class BaseOAuth(object):
@@ -102,74 +103,120 @@ class BaseOAuth(object):
             'authorize_url' : services[self.oauth_version]['AUTHORIZE_TOKEN_URL'],
             'base_url': vars(self).get('base_url',None)
         })
+
+        # Defining oauth service
         self.oauth = services[oauth_version]['SERVICE'](**service_params)
         
-
         if vars(self).get('access_token') and vars(self).get('access_token_secret') and vars(self).get('session_handle'):
             if not self.token_is_valid():
-                self.session = self.refresh_token() 
+                #self.session = self.refresh_token() 
+                json_data.update(self.refresh_token())
+        elif vars(self).get('access_token') and vars(self).get('token_type') and vars(self).get('refresh_token'):
+            if not self.token_is_valid():
+                #self.session = self.refresh_token()
+                json_data.update(self.refresh_token)
         else:
-            if self.oauth_version == 'oauth1':
-                self.oauth1_handler()
-            else:
-                self.oauth2_handler()
-            
-        self.session = self.oauth.get_session((self.access_token, self.access_token_secret))
-
-        json_data.update({
-            'access_token' : self.access_token,
-            'access_token_secret' : self.access_token_secret,
-            'session_handle' : self.session_handle,
-            'token_time' : self.token_time
-        })
+            json_data.update(self.handler()) 
+        
+        # Getting session
+        if self.oauth_version == 'oauth1':
+            self.session = self.oauth.get_session((self.access_token, self.access_token_secret))
+        else:
+            self.session = self.oauth.get_auth_session(data={'code':verifier, 'redirect_uri': self.callback_uri}, decoder=json.loads)
 
         json_write_data(json_data, self.from_file)
 
-    def oauth1_handler(self,):
-        """get request token, redirect user and get access tokens
-        """
-        request_token, request_token_secret = self.oauth.get_request_token(params={'oauth_callback': self.callback_uri})
-        logging.debug("REQUEST_TOKEN = {0}\n REQUEST_TOKEN_SECRET = {1}\n".format(request_token, request_token_secret))
 
-        authorize_url = self.oauth.get_authorize_url(request_token)
-        logging.debug(authorize_url)
+    def handler(self,):
+        """* get request token if OAuth1
+            * Get user authorization
+            * Get access token
+        """
+
+        if self.oauth_version == 'oauth1':
+            request_token, request_token_secret = self.oauth.get_request_token(params={'oauth_callback': self.callback_uri})
+            logging.debug("REQUEST_TOKEN = {0}\n REQUEST_TOKEN_SECRET = {1}\n".format(request_token, request_token_secret))
+            authorize_url = self.oauth.get_authorize_url(request_token)
+        else:
+            authorize_url = self.oauth.get_authorize_url(client_secret=self.consumer_secret, redirect_uri=self.callback_uri, response_type='code')
+
+        logging.debug("AUTHORISATION URL : {0}".format(authorize_url))
+        # Open authorize_url
         webbrowser.open(authorize_url)
         verifier = input("Enter verifier : ")
-        logging.debug("VERIFIER = {0}".format(verifier))
 
         self.token_time = time.time()
-        raw_access = self.oauth.get_raw_access_token(request_token, request_token_secret, params={"oauth_verifier": verifier})
-        parsed_acess = parse_utf8_qsl(raw_access.content)
+    
+        credentials = {'token_time': self.token_time}
+        
+        if self.oauth_version == 'oauth1':
+            raw_access = self.oauth.get_raw_access_token(request_token, request_token_secret, params={"oauth_verifier": verifier})
+            parsed_access = parse_utf8_qsl(raw_access.content)
 
-        self.access_token = parsed_acess['oauth_token']
-        self.access_token_secret = parsed_acess['oauth_token_secret']
-        self.session_handle = parsed_acess['oauth_session_handle']
+            self.access_token = parsed_access['oauth_token']
+            self.access_token_secret = parsed_access['oauth_token_secret']
+            self.session_handle = parsed_access['oauth_session_handle']
+            
+            # Updating credentials 
+            credentials.update({
+                'access_token': self.access_token,
+                'access_token_secret': self.access_token_secret,
+                'session_handle': self.session_handle
+            })
+        else:
+            # Building headers 
+            headers = self.generate_oauth2_headers()
+            # Getting access token
+            raw_access = self.oauth.get_raw_access_token(data={"code": verifier, 'redirect_uri': self.callback_uri,'grant_type':'authorization_code'}, headers=headers)
+            #parsed_access = parse_utf8_qsl(raw_access.content.decode('utf-8'))
+            credentials.update(self.oauth2_access_parser(raw_access))
+                    
+        return credentials
 
-    def oauth2_handler(self,):
+    def generate_oauth2_headers(self):
+        """Generates header for oauth2
         """
-        """
-        url = self.oauth.get_authorize_url(client_secret=self.consumer_secret, redirect_uri=self.callback_uri, response_type='code')
-        logging.debug("AUTHORISATION URL : {0}".format(url))
-        webbrowser.open(url)
-        verifier = input("Enter verifier : ")
-        logging.debug("VERIFIER = {0}".format(verifier))
         encoded_credentials = base64.b64encode(('{0}:{1}'.format(self.consumer_key,self.consumer_secret)).encode('utf-8'))
         headers={'Authorization':'Basic {0}'.format(encoded_credentials.decode('utf-8'))}
-        raw_access = self.oauth.get_raw_access_token(data={"code": verifier, 'redirect_uri': self.callback_uri,'grant_type':'authorization_code'}, headers=headers)
-        parsed_access = parse_utf8_qsl(raw_access.content.decode('utf-8'))
-        pdb.set_trace() 
-        self.oauth.get_auth_session(data={'code':verifier, 'redirect_uri': self.callback_uri})
+
+        return headers
+
+    def oauth2_access_parser(self, raw_access):
+        """Parse oauth2 access
+        """
+        parsed_access = json.loads(raw_access)
+        self.access_token = parsed_access['access_token']
+        self.token_type = parsed_access['token_type']
+        self.refresh_token = parsed_access['refesh_token']
+
+        credentials = {
+            'access_token': self.access_token,
+            'token_type': self.token_type,
+            'refresh_token': self.refresh_token
+        }
+        
+        return credentials
 
     def refresh_token(self,):
         """Refresh access token
         """
         logging.debug("REFRESHING TOKEN")
         self.token_time = time.time()
-        self.access_token, self.access_token_secret = self.oauth.get_access_token(self.access_token, self.access_token_secret, params={"oauth_session_handle": self.session_handle})
+        if self.oauth_version == 'oauth1':
+            self.access_token, self.access_token_secret = self.oauth.get_access_token(self.access_token, self.access_token_secret, params={"oauth_session_handle": self.session_handle})
+            credentials = {
+                'access_token': self.access_token,
+                'access_token_secret': self.access_token_secret,
+                'session_handle': self.session_handle,
+                'token_time': self.token_time
+            }
+        else:
+            headers = self.generate_oauth2_headers()
 
-        session = self.oauth.get_session((self.access_token, self.access_token_secret))
+            raw_access = self.oauth.get_raw_access_token(data={"refresh_token": self.refresh_token, 'redirect_uri': self.callback_uri,'grant_type':'refresh_token'}, headers=headers)
+            credentials.update(self.oauth2_access_parser(raw_access))            
 
-        return session
+        return credentials
 
     def token_is_valid(self,):
         """Check the validity of the token :3600s
