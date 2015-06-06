eac3c89d3dd281747eb963786fd530b1db242c4e
josuebrunel <josuebrunel@gmail.com>
2015-06-06 02:45:38 +0200

    fix #6: OAuth2 support added

--- yahoo_oauth.py
+++ yahoo_oauth.py
@@ -122,7 +122,9 @@ class BaseOAuth(object):
         if self.oauth_version == 'oauth1':
             self.session = self.oauth.get_session((self.access_token, self.access_token_secret))
         else:
-            self.session = self.oauth.get_auth_session(data={'code':verifier, 'redirect_uri': self.callback_uri}, decoder=json.loads)
+            #self.session = self.oauth.get_auth_session(data={'code': self.verifier, 'redirect_uri': self.callback_uri}, decoder=lambda b: json.loads(str(b)))
+            #self.session = self.oauth.get_auth_session(data={'code': self.verifier, 'redirect_uri': self.callback_uri}, decoder=self.custom_decoder)
+            self.session = self.oauth.get_session(token=self.access_token)
 
         json_write_data(json_data, self.from_file)
 
@@ -143,7 +145,7 @@ class BaseOAuth(object):
         logging.debug("AUTHORISATION URL : {0}".format(authorize_url))
         # Open authorize_url
         webbrowser.open(authorize_url)
-        verifier = input("Enter verifier : ")
+        self.verifier = input("Enter verifier : ")
 
         self.token_time = time.time()
     
@@ -167,7 +169,7 @@ class BaseOAuth(object):
             # Building headers 
             headers = self.generate_oauth2_headers()
             # Getting access token
-            raw_access = self.oauth.get_raw_access_token(data={"code": verifier, 'redirect_uri': self.callback_uri,'grant_type':'authorization_code'}, headers=headers)
+            raw_access = self.oauth.get_raw_access_token(data={"code": self.verifier, 'redirect_uri': self.callback_uri,'grant_type':'authorization_code'}, headers=headers)
             #parsed_access = parse_utf8_qsl(raw_access.content.decode('utf-8'))
             credentials.update(self.oauth2_access_parser(raw_access))
                     
@@ -181,13 +183,25 @@ class BaseOAuth(object):
 
         return headers
 
+    def custom_decoder(self, data, encoding='utf-8'):
+        """Custom decoder for auth_session
+        """
+        import pdb
+        pdb.set_trace()
+        result = data.decode(encoding)
+        if result:
+            json_data = json.loads(result)
+            return json_data
+        else:
+            return {}
+
     def oauth2_access_parser(self, raw_access):
         """Parse oauth2 access
         """
-        parsed_access = json.loads(raw_access)
+        parsed_access = json.loads(raw_access.content.decode('utf-8'))
         self.access_token = parsed_access['access_token']
         self.token_type = parsed_access['token_type']
-        self.refresh_token = parsed_access['refesh_token']
+        self.refresh_token = parsed_access['refresh_token']
 
         credentials = {
             'access_token': self.access_token,
