26fd61a917cb590396c88718e1edd328c826f784
Josue Kouka <josuebrunel@gmail.com>
2015-07-25 18:38:53 +0200

    reoganazing the project

--- yahoo_oauth/utils.py
+++ yahoo_oauth/utils.py
@@ -1,6 +1,23 @@
 from __future__ import absolute_import, unicode_literals
 
 import json
+from rauth import OAuth1Service, OAuth2Service
+
+services = {
+    'oauth1': dict(
+        SERVICE = OAuth1Service,
+        REQUEST_TOKEN_URL = "https://api.login.yahoo.com/oauth/v2/get_request_token",
+        ACCESS_TOKEN_URL = "https://api.login.yahoo.com/oauth/v2/get_token",
+        AUTHORIZE_TOKEN_URL = "https://api.login.yahoo.com/oauth/v2/request_auth"
+    ),
+    'oauth2': dict(
+        SERVICE = OAuth2Service,
+        AUTHORIZE_TOKEN_URL = "https://api.login.yahoo.com/oauth2/request_auth",
+        ACCESS_TOKEN_URL = "https://api.login.yahoo.com/oauth2/get_token"
+    )
+}
+
+CALLBACK_URI = 'oob'
 
 def json_write_data(json_data, filename):
     """Write json data into a file
