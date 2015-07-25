38c46edd581297945a0e358483ccf909173d6e20
Josue Kouka <josuebrunel@gmail.com>
2015-07-25 18:27:22 +0200

    #1: improving documentation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 __author__ = 'Josue Kouka'
 __email__ = 'josuebrunel@gmail.com'
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 #requirements.txt
 with open('requirements.txt') as f:
