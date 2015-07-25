fcce25c7de4649fb80dc947456fdba376b55cd9b
Josue Kouka <josuebrunel@gmail.com>
2015-07-25 18:38:08 +0200

    Improving tests

--- tests.py
+++ tests.py
@@ -9,7 +9,8 @@ from yahoo_oauth.yahoo_oauth import json_write_data, json_get_data
 from yahoo_oauth import OAuth1, OAuth2
 
 logging.basicConfig(level=logging.DEBUG,format="[%(asctime)s %(levelname)s] [%(name)s.%(module)s.%(funcName)s] %(message)s \n")
-oauth_logger = logging.getLogger('yahoo-oauth')
+oauth_logger = logging.getLogger('yahoo_oauth')
+
 #oauth_logger.disabled = True
 
 class TestYahooOAuth(unittest.TestCase):
