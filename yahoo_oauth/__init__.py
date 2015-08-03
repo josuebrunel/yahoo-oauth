73fcdfd2e1091e60ef3ab8190e3d4f736b139f18
Josue Kouka <josuebrunel@gmail.com>
2015-08-03 11:23:31 +0200

    fix #35 : xoauth_yahoo_guid now saved in credential file

--- yahoo_oauth/__init__.py
+++ yahoo_oauth/__init__.py
@@ -1,4 +1,9 @@
 from __future__ import absolute_import
 
+__author__ = 'Josue Kouka'
+__email__ = 'josuebrunel@gmail.com'
+__version__ = "0.1.6"
+
+
 from yahoo_oauth.yahoo_oauth import OAuth1, OAuth2
 from yahoo_oauth.logger import YahooLogger
