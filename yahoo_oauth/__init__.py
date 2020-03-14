678972fdefb1969caff63ff0e558f70b57f9d583
Josue Kouka <jkouka@scaleway.com>
2020-03-14 23:13:15 +0100

    yahoo_oauth: rename yahoo_oauth.py file in oauth.py

--- yahoo_oauth/__init__.py
+++ yahoo_oauth/__init__.py
@@ -1,9 +1,5 @@
 from __future__ import absolute_import
 
-__author__ = 'Josue Kouka'
-__email__ = 'josuebrunel@gmail.com'
-__version__ = "0.1.9"
 
-
-from yahoo_oauth.yahoo_oauth import OAuth1, OAuth2
+from yahoo_oauth.oauth import OAuth1, OAuth2
 from yahoo_oauth.logger import YahooLogger
